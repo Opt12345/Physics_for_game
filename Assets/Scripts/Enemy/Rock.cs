a1e1bf5e0aad534b705e86934ae8d3c051738c7f
Opt <opttsorasit@gmail.com>
2024-04-28 21:22:28 +0700

    Add mace

--- Assets/Scripts/Enemy/Rock.cs
+++ Assets/Scripts/Enemy/Rock.cs
@@ -1,11 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class Rock : MonoBehaviour
 {
+    public float raycastDistance = 1f;  // ระยะทางของ Raycast
+    public Color rayColor = Color.red;  // สีของเส้น Raycast
+    public float gravityOnPlayerPassed = 4f;  // ค่า Gravity เมื่อ Player ผ่าน Raycast
+
+    private Rigidbody2D rb;  // Rigidbody ของ Rock
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0f;  // เซ็ต Gravity ของ Rock เป็น 0 ตั้งแต่เริ่มเกม
+    }
+
     void Update()
     {
+        // สร้าง Raycast ในทิศทางลง (Vector3.down)
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, raycastDistance);
+
+        // วาดเส้น Raycast ใน Scene
+        Debug.DrawRay(transform.position, Vector3.down * raycastDistance, rayColor);
+
+        // ตรวจสอบว่า Raycast ตรงกับ Player หรือไม่
+        if (hit.collider != null && hit.collider.CompareTag("Player"))
+        {
+            Debug.Log("Fall!!");
+            
+            // เปลี่ยนค่า Gravity ของ Rock เมื่อ Player ผ่าน Raycast
+            rb.gravityScale = gravityOnPlayerPassed;
+        }
     }
 }
