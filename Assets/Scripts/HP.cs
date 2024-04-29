7665553613c259c8dc47ecdec1bcf86d097af657
ShaoKuy <iqtanawad@gmail.com>
2024-04-29 14:14:01 +0700

    Script HP

---
+++ Assets/Scripts/HP.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HP : MonoBehaviour
+{
+    public int maxRespawns = 3; // จำนวนครั้งที่สามารถ Respawn ได้
+    private int respawnCount; // จำนวนครั้งที่เหลือ
+
+    public TextMeshProUGUI respawnCountText; // Reference ไปยัง Text Element ที่แสดงจำนวนครั้งที่เหลือ
+
+    void Start()
+    {
+        respawnCount = maxRespawns; // กำหนดค่าจำนวนครั้งที่เหลือให้เท่ากับค่าสูงสุดเมื่อเริ่มเกม
+        UpdateRespawnCountUI(); // อัปเดต UI เพื่อแสดงจำนวนครั้งที่เหลือ
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy")) // ตรวจสอบว่าเกิดการชนกับศัตรูหรือไม่
+        {
+            respawnCount--; // ลบจำนวนครั้งที่เหลือลง 1
+            UpdateRespawnCountUI(); // อัปเดต UI เพื่อแสดงจำนวนครั้งที่เหลือ
+
+            if (respawnCount <= 0) // ถ้าไม่มีครั้งที่เหลือ
+            {
+                Debug.Log("Game Over");
+            }
+            else
+            {
+                Respawn(); // Respawn ตัวละคร
+            }
+        }
+    }
+
+    void Respawn()
+    {
+        SceneManager.LoadScene("MapForPlay");
+    }
+
+    void UpdateRespawnCountUI()
+    {
+        respawnCountText.text = "x " + respawnCount.ToString(); // อัปเดต Text บน UI เพื่อแสดงจำนวนครั้งที่เหลือ
+    }
+}
